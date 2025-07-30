73f98f64ba22448a27d212cac44bef33b6528da3
Local-Demo <local@demo>
2025-07-30 14:15:07 +0300

    inital insecure code

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# 🚨 Hardcoded secret (Semgrep + Gitleaks detect)
+API_KEY = "sk_live_1234567890abcdef"
+
+@app.route("/search")
+def search():
+    query = request.args.get("q")
+    # 🚨 SQL injection (Semgrep detect)
+    return f"SELECT * FROM users WHERE name = '{query}'"
+
+@app.route("/")
+def index():
+    return "Hello insecure world!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
